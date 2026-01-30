8629f325170a68430606ccdcb73d6bf2c6a48e27
goda emad <goda.emade2001@gmail.com>
2026-01-30 18:25:09 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="FC Barcelona Analytics", layout="wide")
+
+st.title("🔵🔴 FC Barcelona Analytics Dashboard")
+
+st.success("App is running successfully 🚀")
 
